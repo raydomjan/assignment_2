f3326066ef33d857a65da23ff06364e24b9963f0
Nnamdi Ezeigwe <NnamdiEzeigwe@users.noreply.github.com>
2020-04-01 01:08:16 -0400

    changes

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const bttn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 for(let i = 1; i <= 5; i++) {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+    const newImage = document.createElement('full-img');
+    newImage.setAttribute('src', 'pics' + i + '.jpg');
     thumbBar.appendChild(newImage);
 
     newImage.onclick = function(n) {
@@ -20,6 +20,11 @@ btn.onclick =function() {
         btn.setAttribute('class','light');
         btn.textContent = 'Lighten';
         overlay.getElementsByClassName.backgroundColor = "rgba(0,0,0,0.5)"
+    }
+    else{
+        btn.setAttribute('class', 'shade');
+        btn.textContent = 'Darken';
+        overlay.getElementsByClassName.backgroundColor ='rgba(0,0,0,0)'
     }
 }
 
